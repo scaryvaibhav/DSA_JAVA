7ebfb1a0f20a0291d879a2075d206d2afaffca76
Vaibhav Singh <vaibhav.singh4806@gmail.com>
2026-09-10 17:48:06 +0530

    Arrays as function arguments

---
+++ Arrays/array_function.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class array_function {
+    public static void update(int marks[]){
+        for (int i=0;i<marks.length;i++){
+            marks[i]+=1;
+        }
+    }
+    public static void main (String args[]){
+        int marks[] = {98,95,89};
+        update(marks);
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+       }
+    }
+    
+}
